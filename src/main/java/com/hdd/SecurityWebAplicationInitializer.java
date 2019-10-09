7c4214279dabdafafbc1aa305cf02f3c64bfc713
duy <duyme18@gmail.com>
2019-10-10 00:59:30 +0700

    add project

---
+++ src/main/java/com/hdd/SecurityWebAplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.hdd;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
